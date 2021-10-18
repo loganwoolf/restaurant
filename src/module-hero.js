68b1a31e9f64a980b1556dac17e13cb1ed8c3c78
Logan Woolf <loganwoolf@gmail.com>
2021-10-17 22:28:53 -0600

    add buttons to hero

--- src/module-hero.js
+++ src/module-hero.js
@@ -9,6 +9,24 @@ const emblem = () => {
    return emblem
 }
 
+const buttons = () => {
+   const buttonsContainer = document.createElement('div')
+   const buttonList = ['home', 'menu', 'contact']
+
+   buttonList.forEach( (button) => {
+      const element = document.createElement('button')
+      element.classList.add('nav-button')
+      element.classList.add(`btn-${button}`)
+      element.innerText = `${button}`
+
+      buttonsContainer.appendChild(element)
+   })
+
+   buttonsContainer.classList.add('buttons-container')
+
+
+   return buttonsContainer
+}
 
 const hero = () => {
 	const hero = document.createElement('div')
@@ -24,6 +42,7 @@ const hero = () => {
 	banner.appendChild(emblem())
 	banner.appendChild(logo)
 	hero.appendChild(banner)
+   hero.appendChild(buttons())
 
 	return hero
 }
