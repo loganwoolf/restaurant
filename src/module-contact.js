1a22f023e8c8b9ff9723d1189b2febbb60d4bbc7
Logan Woolf <loganwoolf@gmail.com>
2021-10-19 21:01:58 -0600

    add photos to home tab, tweak layout

--- src/module-contact.js
+++ src/module-contact.js
@@ -1,5 +1,24 @@
 const contact = () => {
+	const people = document.createElement('div')
+	people.classList.add('people')
+	const p = document.createElement('p')
+	p.innerText = 'Here are some people'
+	p.classList.add('page-subtext')
+	people.appendChild(p)
 
+	const manager = document.createElement('div')
+
+	const owner = document.createElement('div')
+
+	return people
 }
+
+const personFactory = (name, position, number) => {
+	return {name, position, number}
+}
+
+const manager = personFactory('Kevin Jager', 'Manager', '234-567-8901')
+const owner = personFactory('Torsten Pabst', 'Manager / Owner', '234-567-8987')
+
 
 export { contact }
--- src/module-home.js
+++ src/module-home.js
@@ -1,12 +1,38 @@
+import Cake from './images/cake.jpg'
+import Coffee from './images/coffee.jpg'
+import IceCream from './images/ice-cream.jpg'
+import Pasta from './images/pasta.jpg'
+
+
 const home = () => {
 	const home = document.createElement('div')
 	const p = document.createElement('p')
 
 	home.classList.add('home')
+	p.classList.add('page-subtext')
 
 	p.innerText = "Come in and feel truly inspired! Enjoy our calm and relaxing atmosphere with ethically sourced coffee and culinary delights for any time of day."
 
 	home.appendChild(p)
+
+	const imageContainer = document.createElement('div')
+	imageContainer.classList.add('home-image-container')
+
+	const imgCake = new Image()
+	imgCake.src = Cake
+	const imgCoffee = new Image()
+	imgCoffee.src = Coffee
+	const imgIceCream = new Image()
+	imgIceCream.src = IceCream
+	const imgPasta = new Image()
+	imgPasta.src = Pasta
+	
+	imageContainer.appendChild(imgCoffee)
+	imageContainer.appendChild(imgPasta)
+	imageContainer.appendChild(imgCake)
+	imageContainer.appendChild(imgIceCream)
+
+	home.appendChild(imageContainer)
 
 	return home
 }
